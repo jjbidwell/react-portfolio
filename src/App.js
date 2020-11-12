c9bea4d3f7c5090f0b3e595a25288787665b0b56
Joshua Bidwell <jjbidwell262@gmail.com>
2020-11-12 12:49:36 -0800

    Added Hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import './App.css';
 import Header from "../src/components/Header/header"
 import Wrapper from "../src/components/Wrapper/wrapper"
